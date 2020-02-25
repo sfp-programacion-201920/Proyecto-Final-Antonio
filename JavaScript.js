6113c433daf11a7af8f4a73bbc6d8c812935877d
Antonio Jiménez <antoniojimenezriquelme@outlook.es>
2020-02-25 12:57:36 +0100

    Modificaciones Criterio A

--- JavaScript.js
+++ JavaScript.js
@@ -1,2 +1,37 @@
-// alert("Bienvenido a mi página sobre la Formula 1");
-//Aquí estoy poniendo un pop-up con JavaScript en el cual doy la bienvenida a mi página web
+    // alert("Bienvenido a mi página sobre la Formula 1");
+    //Aquí estoy poniendo un pop-up con JavaScript en el cual doy la bienvenida a mi página web
+    function form_completo() {
+        var nombre = document.forms["formulario"]["apodo"].value;
+        if (nombre === null || nombre === "") {
+            alert("Te faltan campos por rellenar");
+            return false;
+        }
+    }
+    function valid_password() {
+        var min_length = document.forms["formulario"]["telefono"].value;
+        if (min_length < 5) {
+            alert("La contaseña es demasiado corta");
+            return false;
+        }
+    }
+    function valid_correo() {
+        var email = document.forms["formulario"]["correo"].value;
+        if (isNaN(correo)) {
+            alert("El correo contiene caracteres no permitidos, prueba de nuevo");
+            return false;
+        }
+    }
+    function warning_password() {
+        var password = document.forms["formulario"]["contraseña"].value;
+        if (password < 2){
+        alert("Cuidado con la contraseña que eliges ya que no la podras cambiar");
+        return false;
+        }
+    }
+    function driver() {
+        var piloto = document.forms["formulario"]["Mclaren_F1"].value;
+        if (piloto === "Mclaren") {
+            alert("Has seleccionado al mejor piloto");
+            return false;
+        }
+    }
