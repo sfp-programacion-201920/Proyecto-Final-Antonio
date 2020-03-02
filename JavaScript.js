cc117f27235ae1005253bf7526aa986a09b7d3c8
antjimriq <antoniojimenezriquelme@outlook.es>
2020-03-02 23:05:21 +0100

    Subida final criterio A

--- JavaScript.js
+++ JavaScript.js
@@ -1,37 +1,38 @@
-    // alert("Bienvenido a mi página sobre la Formula 1");
-    //Aquí estoy poniendo un pop-up con JavaScript en el cual doy la bienvenida a mi página web
-    function form_completo() {
-        var nombre = document.forms["formulario"]["apodo"].value;
-        if (nombre === null || nombre === "") {
-            alert("Te faltan campos por rellenar");
-            return false;
-        }
-    }
-    function valid_password() {
-        var min_length = document.forms["formulario"]["telefono"].value;
-        if (min_length < 5) {
-            alert("La contaseña es demasiado corta");
-            return false;
-        }
-    }
-    function valid_correo() {
-        var email = document.forms["formulario"]["correo"].value;
-        if (isNaN(correo)) {
-            alert("El correo contiene caracteres no permitidos, prueba de nuevo");
-            return false;
-        }
-    }
-    function warning_password() {
-        var password = document.forms["formulario"]["contraseña"].value;
-        if (password < 2){
-        alert("Cuidado con la contraseña que eliges ya que no la podras cambiar");
+// alert("Bienvenido a mi página sobre la Formula 1");
+//Aquí estoy poniendo un pop-up con JavaScript en el cual doy la bienvenida a mi página web
+function form_completo() {
+    var nombre = document.forms["formulario"]["nombre"].value;
+    if (nombre === null || nombre === "") {
+        alert("Te falta por rellenar el nombre");
         return false;
-        }
     }
-    function driver() {
-        var piloto = document.forms["formulario"]["Mclaren_F1"].value;
-        if (piloto === "Mclaren") {
-            alert("Has seleccionado al mejor piloto");
-            return false;
-        }
+}
+function valid_password() {
+    var contraseña = document.forms["formulario"]["contraseña"].value;
+    if (contraseña == 0) {
+        alert("La contaseña no puede quedar vacia");
+        return false;
     }
+}
+function valid_correo() {
+    var email = document.forms["formulario"]["correo"].value;
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(email))) {
+        alert("El correo solo puede contener caracteres permitidos");
+        return false;
+    }
+}
+function warning_password() {
+    var contraseña1 = document.forms["formulario"]["contraseña"].value;
+    var contraseña2 = document.forms["formulario"]["contraseña2"].value;
+    if (contraseña1 != contraseña2) {
+        alert("Las contraseñas no coinciden");
+        return false;
+    } else {
+        alert("Las contraseñas coinciden");
+        return false;
+    }
+}
+function profilepic() {
+    alert("Seleccione una imagen");
+    return false;
+}
